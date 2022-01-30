6ff48359c8e31d4ba82876de4528a232bbc3314f
thakur-santosh <santoshthakur001100@gmail.com>
2022-01-30 22:38:07 +0530

    updated router navigation between apps

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
 import App from './App';
 
 // Mount function to start up the app
-const mount = (el) => {
-  ReactDOM.render(<App />, el);
+const mount = (el, { onNavigate, defaultHistory }) => {
+  const history = defaultHistory || createMemoryHistory();
+  onNavigate && history.listen(onNavigate);
+  ReactDOM.render(<App history={history} />, el);
+
+  return {
+    onParentNavigate({ pathname: nextPathName }) {
+      const { pathname } = history.location;
+      if (pathname !== nextPathName) history.push(nextPathName);
+    },
+  };
 };
 
 // If we are running this application in isolation
 // call mount immediately
 if (process.env.NODE_ENV === 'development') {
   const devRoot = document.querySelector('#_marketing-dev-root');
-  if (devRoot) mount(devRoot);
+  if (devRoot) mount(devRoot, { defaultHistory: createBrowserHistory() });
 }
 
 export { mount };
